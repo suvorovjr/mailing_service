5a2b77874e49e907fadd041195684672e471526c
suvorovjr <pglavan1998@mail.ru>
2024-02-20 11:30:48 +0300

    add profile

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from users.apps import UsersConfig
-from users.views import LoginView, UserCreateView, activate_user
+from users.views import LoginView, UserCreateView, ProfileView, activate_user
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path('auth/', UserCreateView.as_view(), name='auth'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('profile/', ProfileView.as_view(), name='profile'),
     path('confirm/<str:token>', activate_user, name='confirm')
 ]
--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.contrib.sites.shortcuts import get_current_site
 from django.utils.crypto import get_random_string
 from django.shortcuts import redirect
 from users.services import send_verification_password
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from django.urls import reverse, reverse_lazy
 from users.models import User
 from users.forms import UserForm, LoginForm
@@ -27,6 +27,14 @@ class UserCreateView(CreateView):
         verification_link = f'http://{get_current_site(self.request)}/users/confirm/{self.object.verification_token}'
         send_verification_password(self.object.email, verification_link)
         return super().form_valid(form)
+
+
+class ProfileView(DetailView):
+    model = User
+    template_name = 'users/profile.html'
+
+    def get_object(self, queryset=None):
+        return self.request.user
 
 
 def activate_user(request, token):
