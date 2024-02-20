3aaac2669179ba0c5a1db518d0fc7eed5ffc6268
suvorovjr <pglavan1998@mail.ru>
2024-02-20 10:36:11 +0300

    add my_avatar

--- users/templatetags/users_tags.py
+++ users/templatetags/users_tags.py
@@ -6,3 +6,10 @@ register = template.Library()
 @register.filter(name='add_id')
 def add_id(subject_form, new_id):
     return subject_form.as_widget(attrs={'id': new_id})
+
+
+@register.filter(name='my_avatar')
+def my_avatar(imagine):
+    if imagine:
+        return f'/media/{imagine}'
+    return '/static/imagine/avatar.jpeg'
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from users.apps import UsersConfig
 from users.views import LoginView, UserCreateView, activate_user
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 app_name = UsersConfig.name
@@ -7,5 +8,6 @@ app_name = UsersConfig.name
 urlpatterns = [
     path('auth/', UserCreateView.as_view(), name='auth'),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('confirm/<str:token>', activate_user, name='confirm')
 ]
