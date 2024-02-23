70c6c67161124c256b18d00ed49c7213ed46ebcf
suvorovjr <pglavan1998@mail.ru>
2024-02-23 16:47:03 +0300

    add blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from users.models import NULLABLE
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    description = models.TextField(verbose_name='Описание')
+    slug = models.CharField(max_length=100, verbose_name='Слаг', **NULLABLE)
+    imagine = models.ImageField(upload_to='blog/', verbose_name='Изображение', **NULLABLE)
+    count_views = models.IntegerField(default=0, verbose_name='Количество просмотров')
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    is_publish = models.BooleanField(default=True, verbose_name='Активность')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Новость"
+        verbose_name_plural = "Новости"
