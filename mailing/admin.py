70b23166232493436b3b89844babbb347baf820b
suvorovjr <pglavan1998@mail.ru>
2024-03-01 19:58:31 +0300

    add Log Admin

--- mailing/admin.py
+++ mailing/admin.py
@@ -5,3 +5,8 @@ from mailing.models import Mailing, Log, Client
 @admin.register(Mailing)
 class MailingAdmin(admin.ModelAdmin):
     list_display = ('start_mail', 'end_mail', 'mail_time', 'status_mail', 'user')
+
+
+@admin.register(Log)
+class LogAdmin(admin.ModelAdmin):
+    list_display = ('date_of_mail', 'status_of_mail', 'mailing',)
