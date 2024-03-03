bca9e73e247e322d658faae830279b7dfba74a9c
suvorovjr <pglavan1998@mail.ru>
2024-03-03 21:03:33 +0300

    add cache to blog list view

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from blog.apps import BlogConfig
 from django.urls import path
+from django.views.decorators.cache import cache_page
 from blog.views import BlogListView, BlogCreateView, BlogDeleteView, BlogDetailView, BlogUpdateView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path('', BlogListView.as_view(), name='list'),
+    path('', cache_page(60)(BlogListView.as_view()), name='list'),
     path('create', BlogCreateView.as_view(), name='create'),
     path('update/<slug:slug>/', BlogUpdateView.as_view(), name='update'),
     path('detail/<slug:slug>/', BlogDetailView.as_view(), name='detail'),
