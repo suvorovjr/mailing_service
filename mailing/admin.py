bca549108f4b1715fe33ff15356b1ae535f006eb
suvorovjr <pglavan1998@mail.ru>
2024-03-03 16:58:26 +0300

    add Client Admin

--- mailing/admin.py
+++ mailing/admin.py
@@ -7,6 +7,11 @@ class MailingAdmin(admin.ModelAdmin):
     list_display = ('start_mail', 'end_mail', 'mail_time', 'status_mail', 'user')
 
 
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('email', 'full_name')
+
+
 @admin.register(Log)
 class LogAdmin(admin.ModelAdmin):
     list_display = ('date_of_mail', 'status_of_mail', 'mailing',)
