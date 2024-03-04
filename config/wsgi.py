4371be01296a5591b27c21fd04bb7d0e2ddc4f69
suvorovjr <pglavan1998@mail.ru>
2024-03-04 15:27:42 +0300

    add shedule settings

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from apscheduler.schedulers.background import BackgroundScheduler
+from mailing.services import send_emails
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 application = get_wsgi_application()
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(send_emails, 'interval', minutes=2)
+scheduler.start()
